0aecbf96f28a5f2560a7c20f197005d8cfff84c0
Adelina Simion <adelina.simion@form3.tech>
2022-09-22 18:12:08 +0100

    cleanup and add menu
    Add load testing
    
    Clean up

--- db/items.go
+++ db/items.go
@@ -3,29 +3,44 @@ package db
 import "fmt"
 
 type InventoryService struct {
-	stock map[string]int
+	stock map[string]MenuItem
 }
 
-func NewInventoryService(s map[string]int) *InventoryService {
+type MenuItem struct {
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+}
+
+func NewInventoryService(s map[string]MenuItem) *InventoryService {
 	return &InventoryService{stock: s}
 }
 
-func (s *InventoryService) DecrementStock(items []LineItem) error {
+func (s *InventoryService) PlaceOrder(items []LineItem) error {
 	for _, v := range items {
-		stockLevel, ok := s.stock[v.Name]
+		menuItem, ok := s.stock[v.Name]
 		if !ok {
 			return fmt.Errorf("item not found %s", v.Name)
 		}
 
-		if stockLevel < v.Quantity {
-			return fmt.Errorf("insufficent stock, got %d but wanted %d", stockLevel, v.Quantity)
+		if menuItem.Quantity < v.Quantity {
+			return fmt.Errorf("insufficent stock, got %d but wanted %d",
+				menuItem.Quantity, v.Quantity)
 		}
 	}
 
 	for _, v := range items {
-		stockLevel := s.stock[v.Name]
-		stockLevel -= v.Quantity
+		menuItem := s.stock[v.Name]
+		menuItem.Quantity -= v.Quantity
+		s.stock[v.Name] = menuItem
 	}
 
 	return nil
+}
+
+func (s *InventoryService) GetStock() []MenuItem {
+	var items []MenuItem
+	for _, v := range s.stock {
+		items = append(items, v)
+	}
+	return items
 }
